13b8d6eb901687cef2c177d5555fee6930728af4
Mario Torres <mario@zooft.company>
2020-03-24 23:06:02 -0300

    add config ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from '../app/modules/layout/layout.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NgbModule
   
   ],
   imports: [
