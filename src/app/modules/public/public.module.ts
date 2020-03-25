a941b44f594505f556ce2038ad0e77bd63290c3b
Mario Torres <mario@zooft.company>
2020-03-25 16:55:05 -0300

    fix style navbar, footer

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicPageComponent } from './container/public-page.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component'
 import { PublicRoutingModule } from './public.routes';
+//MDB
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { PublicRoutingModule } from './public.routes';
   imports: [
     CommonModule,
     PublicRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   exports:[
     PublicPageComponent
   ]
