f9aa070103d97b4e44e70361742d4c0b76e9f5b4
Mario Torres <mario@zooft.company>
2020-03-25 00:47:25 -0300

    fix errors

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PublicPageComponent } from './container/public-page/public-page.component';
-import { DashboardComponent } from './container/dashboard/dashboard.component';
+import { PublicPageComponent } from './container/public-page.component';
 
 
 
 @NgModule({
-  declarations: [PublicPageComponent, DashboardComponent],
+  declarations: [PublicPageComponent],
   imports: [
     CommonModule
   ]
